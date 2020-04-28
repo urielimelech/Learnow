bf10666cd1f5c8a355e3a946ba573c33fa3f158a
uri <urielimelech@hotmail.com>
2020-04-28 18:33:04 +0300

    create user config in the DB when register

--- Learnow Web Server/controllers/SessionRecorder/ReactSocket/SocketsOnLogic/OnRegisterData.js
+++ Learnow Web Server/controllers/SessionRecorder/ReactSocket/SocketsOnLogic/OnRegisterData.js
@@ -1,21 +1,36 @@
 import axios from 'axios'
 
 import { dbURL } from '../../../../consts.js'
+import { sessionConfig } from '../../SessionConfig.js'
 
-export const onRegisterData = (soc, name, email, password, userType) => {
-    console.log(userType)
-    const body = {
+export const onRegisterData = (soc, userConfigs, name, email, password, userType) => {
+    /** create new user in users collection */
+    const userBody = {
         name: name,
         email: email,
         password: password,
         userType: userType
     }
-    axios.post(`${dbURL}/register`, body)
+    axios.post(`${dbURL}/register`, userBody)
     .then(res => soc.emit('registration data', {email: email, name: name, userType: userType, token: res.data.token, success: res.data.success, message: res.data.message}))
     .catch(err => {
         if (err.response.data.message.hasOwnProperty('message'))
             soc.emit('registration data', {success: err.response.data.success, message: err.response.data.message.message})
         else 
             soc.emit('registration data', {success: err.response.data.success, message: err.response.data.message})
+    })
+
+    /** create new user configuration in userconfigs collection */
+    const config = sessionConfig()
+    const userConfigBody = {
+        userEmail: email,
+        config: config
+    }
+    axios.post(`${dbURL}/addUserConfig`, userConfigBody)
+    .then(res => {
+        userConfigs.push(res.data)
+    })
+    .catch(err => {
+        console.log(err)
     })
 }
