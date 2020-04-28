596dcd076139b2281ba10bcf15906d617d6bfe0c
uri <urielimelech@hotmail.com>
2020-04-28 18:32:15 +0300

    save user config to the DB

---
+++ Learnow Web Server/controllers/SessionRecorder/ReactSocket/SocketsOnLogic/OnSaveConfiguration.js
@@ -0,0 +1,16 @@
+import axios from 'axios'
+
+import { dbURL } from '../../../../consts.js'
+
+export const onSaveConfiguration = (userConfigs, config, userEmail) => {
+    userConfigs.forEach(userConfig => {
+        if (userConfig.userEmail === userEmail) {
+            userConfig.config = config
+            axios.put(`${dbURL}/updateUserConfig`, userConfig)
+            .then(res => console.log('user', userEmail, 'save configuration successfully', res.data))
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    })
+}
