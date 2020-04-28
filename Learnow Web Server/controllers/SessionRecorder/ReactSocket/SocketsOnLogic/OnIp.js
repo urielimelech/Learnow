3a6656aef3b76939bf2601d37ce125fd9db4c8a1
uri <urielimelech@hotmail.com>
2020-04-28 18:35:11 +0300

    save user config in room when user is in room

--- Learnow Web Server/controllers/SessionRecorder/ReactSocket/SocketsOnLogic/OnIp.js
+++ Learnow Web Server/controllers/SessionRecorder/ReactSocket/SocketsOnLogic/OnIp.js
@@ -1,4 +1,4 @@
-export const onIp = (serverIOService, soc, ip) => {
+export const onIp = (serverIOService, soc, rooms, userConfigs, ip, email) => {
     const roomData = serverIOService.sockets.adapter.rooms[ip] 
     /** check if room exists */
     if (!roomData){
@@ -10,5 +10,12 @@ export const onIp = (serverIOService, soc, ip) => {
         console.log('room exist', ip)
         soc.join(ip)
         soc.emit('enter room', true)
+        rooms.forEach(room => {
+            if (room.roomName === ip) {
+                userConfigs.forEach(userConfig => {
+                    room.config = userConfig.config
+                })
+            }
+        })
     }
 }
