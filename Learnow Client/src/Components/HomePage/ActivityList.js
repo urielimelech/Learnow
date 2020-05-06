16ae6d86f7c0b5dc68c707168babcd1c1be72f30
ShohamRoditi <shohamroditi@gmail.com>
2020-05-06 20:33:02 +0300

    activity list + selection components

---
+++ Learnow Client/src/Components/HomePage/ActivityList.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { setActivitiesCards, chooseActivity } from '../../Redux/Actions'
+import { socketToWebServer } from '../../SocketIoClient'
+import { MenuItem } from '@material-ui/core'
+import Select from '@material-ui/core/Select'
+
+export const ActivityList = () => {
+
+    const _dispatch = useDispatch()
+
+    const activitiesCards = useSelector(state => state.MainReducer.activitiesCards)
+    const loggedUser = useSelector(state => state.MainReducer.loggedUser)
+
+    const [activityList, setActivityList] = useState(null)
+    const [activity, setActivity] = useState('')
+
+    const renderActivityList = () => {
+        const activityArr = activitiesCards.map((activity, index) => {
+            return <MenuItem key={index} value={activity.Title}>{activity.Title}</MenuItem>
+        })
+        return [<MenuItem key={activitiesCards.length + 1} value='None'>None</MenuItem>, ...activityArr]
+        // const costumElement = <MenuItem key={activitiesCards.length + 1} value='costum'>Costum Activity</MenuItem>
+        // return [...activityArr, costumElement]
+    }
+
+    useEffect(() => {
+        socketToWebServer.emit('get suggestions cards', loggedUser.email)
+        socketToWebServer.on('suggestions cards', data => {
+            _dispatch(setActivitiesCards(data))
+        })
+        _dispatch(chooseActivity('None'))
+        return () => {
+            socketToWebServer.off('suggestions cards')
+        }
+    },[])
+
+    useEffect(() => {
+        if (activitiesCards) {
+            setActivityList(renderActivityList)
+        }
+    },[activitiesCards])
+
+    // useEffect(() => {
+    //     if (activity === 'costum') {
+    //         handleClickOpen()
+    //     }
+    // },[activity])
+
+    const handleChange = (event) => {
+        setActivity(event.target.value)
+        _dispatch(chooseActivity(event.target.value))
+    }
+
+    return (
+        <Select
+            labelId="demo-simple-select-helper-label"
+            id="demo-simple-select-helper"
+            value={activity}
+            onChange={handleChange}
+        >
+            {activityList}
+        </Select>
+    )
+}
---
+++ Learnow Client/src/Components/HomePage/ActivitySelection.js
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import InputLabel from '@material-ui/core/InputLabel'
+import FormHelperText from '@material-ui/core/FormHelperText'
+import FormControl from '@material-ui/core/FormControl'
+
+// import Button from '@material-ui/core/Button';
+// import Dialog from '@material-ui/core/Dialog';
+// import DialogActions from '@material-ui/core/DialogActions';
+// import DialogContent from '@material-ui/core/DialogContent';
+// import DialogTitle from '@material-ui/core/DialogTitle';
+// import OutlinedInput from '@material-ui/core/OutlinedInput';
+import { ActivityList } from './ActivityList'
+import { Button } from 'react-bootstrap'
+import { navigate } from 'hookrouter'
+
+export const ActivitySelection = () => {
+
+    const useStyles = makeStyles((theme) => ({
+        formControl: {
+          margin: theme.spacing(1),
+          minWidth: 120,
+        },
+        selectEmpty: {
+          marginTop: theme.spacing(2)
+        },
+        container: {
+            display: 'flex',
+            flexWrap: 'wrap'
+        },
+        formControl: {
+            margin: theme.spacing(1),
+            minWidth: 120
+        }
+    }))
+    
+    const classes = useStyles();
+    // const [open, setOpen] = useState(false)
+
+    // const handleClickOpen = () => {
+    //     setOpen(true)
+    // }
+    
+    //   const handleClose = () => {
+    //     setOpen(false)
+    // }
+
+    // const handleCostumChange = event => {
+    //     setCostumActivity(event.target.value)
+    // }
+
+    // const [costumActivity, setCostumActivity] = useState('')
+
+    const saveActivity = () => {
+        navigate('/Session')
+    }
+
+    return (
+        <div>
+            <FormControl className={classes.formControl}>
+                <InputLabel id="demo-simple-select-helper-label">Activity</InputLabel>
+                <ActivityList/>
+                <FormHelperText>Select an activity that you finished right now</FormHelperText>
+            </FormControl>
+            <Button onClick={saveActivity}>Continue</Button>
+            {/* <Dialog disableBackdropClick disableEscapeKeyDown open={open} onClose={handleClose}>
+                <DialogTitle>Fill the form</DialogTitle>
+                <DialogContent>
+                    <FormControl variant="outlined">
+                        <InputLabel htmlFor="component-outlined">Activity</InputLabel>
+                        <OutlinedInput id="component-outlined" value={costumActivity} onChange={handleCostumChange} label="Activity" />
+                    </FormControl>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Cancel
+                    </Button>
+                    <Button onClick={handleClose} color="primary">
+                        Ok
+                    </Button>
+                </DialogActions>
+            </Dialog> */}
+        </div>
+    )
+}
