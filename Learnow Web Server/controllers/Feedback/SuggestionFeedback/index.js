e2807f277656cf8647616d099984675afdb9ada5
ShohamRoditi <shohamroditi@gmail.com>
2020-05-13 20:44:24 +0300

    styling feedback page

--- Learnow Web Server/controllers/Feedback/SuggestionFeedback/index.js
+++ Learnow Web Server/controllers/Feedback/SuggestionFeedback/index.js
@@ -1,18 +1,18 @@
 export const SuggestionFeedback = [
     {
         Title: 'Yoga',
-        TitleCard: '20 minutes of yoga',
-        ImgCard: 'https://png2.cleanpng.com/sh/1d9cbe4e0fd2bb9098425b949b31a3e9/L0KzQYm3WcI4N6FnkJH0aYP2gLBuTgJqa5lmitY2aHn3hL3sjfFve15uhuZ7b3T4c8XwjB4ufJCykdHwYT35dbT7jCIub6MykdHwYT3rdbLzlPguapZzfdhydIOwRX7Aggl0NZpzRelxaXPrPcX2TfVuaqNme9c2NXLkdIW7gcVkbGMASqg3OECzR4G7VcAyPWQ9SKo6OUW6SIW3U75xdpg=/kisspng-richard-hittlemans-introduction-to-yoga-vector-gr-yoga-health-benefits-5-ways-in-which-to-embrace-5bad44a5cd2926.8007045015380819578403.png',
+        TitleCard: 'Yoga',
+        ImgCard: 'yoga',
         DescriptionCard: `Yoga indicates that in addition to reducing the response to stress,
 it is responsible for calming and restoring balance after the stress and pressure have passed.`,
-        Link: 'https://www.youtube.com/watch?v=YoBwfkB7fI0',
+        Link: 'https://www.youtube.com/watch?v=YoBwfkB7fI0&t',
         LinkDescription: 'Yoga Video',
         CardNumber: '1'
     },
     {
         Title: 'Pilates',
-        TitleCard: '20 minutes of Pilates',
-        ImgCard: 'https://png2.cleanpng.com/sh/537e889d790c1b15d57c30bd4bbd9ba2/L0KzQYm3U8I3N6Z1fZH0aYP2gLBuTglwb5Iyi9N7dnHxd7L6gf5iNZ5qfNt9YYTsf7A0kCVzgZIyhtN2YYPuccPoTglwb5IyTdNrOXG1QrS7UvRkP2IzSKs5NkK2Q4O4VcIzOWI6SaMBMke2SXB3jvc=/kisspng-yoga-sarvangasana-meditation-surya-namaskara-yoga-5ab9a22c42dc71.0906233215221151162739.png',
+        TitleCard: 'Pilates',
+        ImgCard: 'pilates',
         DescriptionCard: 'Pilates emphasizes conscious, deep breathing that enhances a sense of relaxation in the body and mind.',
         Link: 'https://www.youtube.com/watch?v=Dhfv6DWQGlk',
         LinkDescription: 'Pilates Video',
@@ -21,7 +21,7 @@ it is responsible for calming and restoring balance after the stress and pressur
     {
         Title: 'Music',
         TitleCard: 'Listening Music',
-        ImgCard: 'https://png2.cleanpng.com/sh/d2b7c8f6ec8b6c285ada819f80655a5f/L0KzQYq3VsAyN5tsR91yc4Pzfri0kP9zfJJnhNc2bnX3h7F5i71oepJ1gNtscz3whcTwgBFtNZ90jNc2aX3kd7a0kCRibpcyhed8aXOwfrF7hgMue6Vmftg2bYX2ebT1jCRme146e9g6ZnOzQIG8UfFmOl84SKQDNEa0SIK8Vck0PmM6TqI5MkC8PsH1h5==/kisspng-portable-network-graphics-musical-note-image-staff-music-notes-staff-musicnotes-5cf1fc00051ae2.3028461815593625600209.png',
+        ImgCard: 'music',
         DescriptionCard: 'Listening Music affects learning and hearing skills.',
         Link: 'https://www.spotify.com/il-en/',
         LinkDescription: 'Listening Music',
@@ -29,11 +29,20 @@ it is responsible for calming and restoring balance after the stress and pressur
     },
     {
         Title: 'Running',
-        TitleCard: 'Running And Aerobic Activity',
-        ImgCard: "https://png2.cleanpng.com/sh/fec18e9429b984ee269d43542881842a/L0KzQYi4UcI4N2U2T5GAYUHmcbTohMU1QWYASZCCMkK1RoW4VME2OWI9SqoBNUO2RIe8TwBvbz==/5a1cacad549591.7222641415118286533465.png",
+        TitleCard: 'Running & Aerobic Activity',
+        ImgCard: 'run',
         DescriptionCard: 'Running and aerobic activity improve the level of attention and memory.',
         Link: 'https://www.youtube.com/watch?v=YlbywsBwmME',
         LinkDescription: 'Running And Aerobic Activity',
         CardNumber: '4'
+    },
+    {
+        Title: 'Playing ',
+        TitleCard: 'Playing Computer Game',
+        ImgCard: 'computerGame',
+        DescriptionCard: 'Computer games improve attention, speed of information processing, past flexibility between different tasks and mental rotation of objects.',
+        Link: 'https://www.miniclip.com/games/en/',
+        LinkDescription: 'Playing Computer Game',
+        CardNumber: '5'
     }
 ]
