75da5e0f82b8058c781074d6d0ac7024a4a2f139
uri <urielimelech@hotmail.com>
2020-05-17 23:28:28 +0300

    fix web server babel bug

--- Learnow Web Server/src/controllers/SessionRecorder/ReactSocket/SocketsOnLogic/OnGetAllUserSessions.js
+++ Learnow Web Server/src/controllers/SessionRecorder/ReactSocket/SocketsOnLogic/OnGetAllUserSessions.js
@@ -2,15 +2,12 @@ import axios from 'axios'
 import { dbURL } from '../../../../dbUrl.js'
 
 export const onGetAllUserSessions = (serverIOService, soc, rooms, userConfigs, email) => {
-    try {
-        const result = axios (
-            `${dbURL}/getAllSessions?userEmail=${email}`
-        )
-        const userSessions = result.data
-        soc.emit('all user sessions', userSessions)
-    }
-    catch (e){
-        console.log('catch', e)
-        return e
-    } 
+        axios.get(`${dbURL}/getAllSessions?userEmail=${email}`)
+        .then(result => {
+            const userSessions = result.data
+            soc.emit('all user sessions', userSessions)
+        })
+        .catch(e => {
+            console.log(e)
+        })
 }
