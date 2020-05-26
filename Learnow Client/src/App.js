1259c02f7df450549907c888c70c0f032e78b189
ShohamRoditi <shohamroditi@gmail.com>
2020-05-26 13:01:22 +0300

    only researcher can change configuration

--- Learnow Client/src/App.js
+++ Learnow Client/src/App.js
@@ -1,7 +1,6 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { useRoutes } from 'hookrouter'
 
-import { NavBar } from './Components/NavBar'
 import Routes from './routes'
 import { BurgerNav } from './Components/NavBar/BurgerNav'
 import { CookiesProvider } from 'react-cookie'
@@ -11,11 +10,8 @@ const App = () => {
   const routeResult = useRoutes(Routes)
   return (
     <CookiesProvider>
-    {/*<NavBar/>*/}
-       {/*routeResult*/}
        <BurgerNav page={routeResult}/>
     </CookiesProvider>
   )
 }
-
 export default App;
