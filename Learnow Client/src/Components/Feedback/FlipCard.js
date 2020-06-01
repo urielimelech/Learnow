9cb68a823522b24c347ff5079165ee6d02967300
ShohamRoditi <shohamroditi@gmail.com>
2020-06-02 00:42:59 +0300

    fix styling in flip card

--- Learnow Client/src/Components/Feedback/FlipCard.js
+++ Learnow Client/src/Components/Feedback/FlipCard.js
@@ -3,7 +3,6 @@ import ReactCardFlip from 'react-card-flip'
 import { Button } from '@material-ui/core'
 import { feedbackStyle } from './FeedbackStyle'
 
-
 export const FlipCard = ({title=null, img=null, onPressSelect=null, description=null, link=null}) => {
     
     const [isFlipped, setIsFlipped] = useState(false)
@@ -15,7 +14,7 @@ export const FlipCard = ({title=null, img=null, onPressSelect=null, description=
 
     return (
         <ReactCardFlip isFlipped={isFlipped} flipDirection="vertical" style={{justifyContent: 'center'}}>
-            <div onClick={handleClick} style={{backgroundColor: '#8DD4D2', borderRadius: '10px', border:' 2px solid #4CC6C4',  height: '300px', width: '320px', margin: '20px'}}>
+            <div onClick={handleClick} style={{backgroundColor: '#8DD4D2', borderRadius: '10px', height: '300px', width: '320px', margin: '20px'}}>
                 <h3>{title}</h3>
                 <img style={{ objectFit: 'contain',  height: '100px', width: '100px'}} src={img}></img>
                 <Button onClick={onPressSelect} style={feedbackStyle.buttonStyle}>select</Button>
