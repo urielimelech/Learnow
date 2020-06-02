c2fa2bbd948bd5325d52668b262e23e29cb09fcc
uri <urielimelech@hotmail.com>
2020-06-02 15:47:18 +0300

    style fix for card components

--- Learnow Client/src/Components/CardComponent/StyledCard.js
+++ Learnow Client/src/Components/CardComponent/StyledCard.js
@@ -3,8 +3,10 @@ import { Card } from '@material-ui/core'
 
 export const StyledCard = styled(Card)`
 && {
-    margin-right: ${props => props.marginr ? props.marginr : null}px;
-    margin-top: ${props => props.margint ? props.margint : null}px;
+    margin-right: ${props => props.marginr ? props.marginr : 0}px;
+    margin-top: ${props => props.margint ? props.margint : 0}px;
+    margin-bottom: ${props => props.marginb ? props.marginb : 0}px;
+    margin-left: ${props => props.marginl ? props.marginl : 0}px;
     width: ${props => props.width ? props.width : 400}px;
     height: ${props => props.height ? props.height : 400}px;
     border: ${props => props.border ? props.border : null};
