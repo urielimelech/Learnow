da5e01d8f5bb5fd4bc5fc7f5353dc60c5d29550d
uri <urielimelech@hotmail.com>
2020-06-08 19:21:08 +0300

    delete junk

--- Learnow Client/src/Components/ResearchUser/index.js
+++ Learnow Client/src/Components/ResearchUser/index.js
@@ -1,111 +1,11 @@
-import React, { useState, useEffect } from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import TextField from '@material-ui/core/TextField'
-import { Button } from '@material-ui/core'
-import FormHelperText from '@material-ui/core/FormHelperText'
-import { dbURL } from '../../consts'
-import axios from 'axios'
-import { TextMessageToastify } from '../TextMessageToastify'
+import React from 'react'
 import { HomePageResearch } from './HomePageResearch'
-import { useDispatch, useSelector } from 'react-redux'
-import { updateStudentForResearch, setActivitiesCards } from '../../Redux/Actions'
-import { socketToWebServer } from '../../SocketIoClient'
 
 export const ResearchUser = () => {
 
-    const useStyles = makeStyles((theme) => ({
-        root: {
-            '& .MuiTextField-root': {
-            margin: theme.spacing(1),
-            width: '50ch',
-            top: 70
-            }
-        }
-    }))
 
-    const classes = useStyles()
-    const _dispatch = useDispatch()
-    const studentData = useSelector(state => state.MainReducer.studentForResearch)
-
-    const [email, setEmail] = useState('')
-    const [error, setError] = useState(false)
-    const [isUserExistsErr, setIsUserExistsErr] = useState(false)
-
-    const handleChange = (event) => {
-        setEmail(event.target.value)
-    }
-
-    const handleSumbit = e => {
-        e.preventDefault()
-        handleClick()
-    }
-
-    const getStudentData = () => {
-        axios.get(`${dbURL}/getStudentData?email=${email}`)
-        .then(res => {
-            _dispatch(updateStudentForResearch(res.data))
-
-            socketToWebServer.emit('get suggestions cards', email)
-            socketToWebServer.on('suggestions cards', data => {
-                _dispatch(setActivitiesCards(data))
-            })
-        })
-        .catch(err => {
-            console.log({err})
-            setIsUserExistsErr(<TextMessageToastify msg={'user is not exists'}/>)
-        })
-    }
-
-    const handleClick = () => {
-        validateEmail()
-        getStudentData()
-    }
-
-    const validateEmail = () => { 
-        const validEmailRegex = 
-        RegExp(/^(([^<>()\],;:\s@]+([^<>()\],;:\s@]+)*)|(.+))@(([^<>()[\],;:\s@]+\.)+[^<>()[\],;:\s@]{2,})$/i)
-        if(email.match(validEmailRegex)){
-            setError(false)          
-        }
-        else {
-            setError(true)
-        }
-    }
-
-    useEffect(() => {
-        if (studentData)
-            setEmail(studentData.email)
-    },[studentData])
-  
     return (
         <div style={{textAlign: 'center'}}>
-            <form className={classes.root} onSubmit={handleSumbit}>
-                <TextField
-                    id="outlined-textarea"
-                    label="Email"
-                    placeholder="Please enter student email"
-                    variant="outlined"
-                    onChange = {handleChange}
-                    value={email}
-                />
-                {error ?
-                    <FormHelperText 
-                        style={{textAlign: 'center', color:'#FF0000', marginTop: '20px'}} 
-                        id="component-error-text"
-                    >
-                        Invalid email
-                    </FormHelperText>
-                    :
-                    null
-                }
-                {isUserExistsErr}
-                <Button 
-                    style={{ margin: '0 auto', backgroundColor:'blue',color: '#ffffff', top: 80, left: 30, height: 50, width: 80}}  
-                    onClick={handleClick}
-                    >
-                        search
-                </Button>
-            </form>
             <HomePageResearch/>
         </div>
     )
