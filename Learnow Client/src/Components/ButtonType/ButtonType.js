8d914f2fe1928d18565e83ad9597c28bd219d27b
ShohamRoditi <shohamroditi@gmail.com>
2020-06-08 20:41:14 +0300

    update generic button component

--- Learnow Client/src/Components/ButtonType/ButtonType.js
+++ Learnow Client/src/Components/ButtonType/ButtonType.js
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { Button, OutlinedInput } from '@material-ui/core'
 
-export const ButtonType = ({children, style, ...props}) => {
-        return <Button style={style}>{children}</Button>
+export const ButtonType = ({children, style, onClick, ...props}) => {
+
+        return <button  style={{...style, border : '2px solid #35C2C0', outline: 0, borderRadius: 5, background: 'linear-gradient(to right, #0ce9ed, #35C2C0)', minWidth: 100, minHeight: 40}} onClick={onClick}>{children}</button>
 }
