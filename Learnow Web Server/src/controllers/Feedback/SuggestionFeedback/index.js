ccc30c46eb0955fb8a62da6c21cb354f79305d7b
ShohamRoditi <shohamroditi@gmail.com>
2020-06-17 00:43:13 +0300

    update feedback style

--- Learnow Web Server/src/controllers/Feedback/SuggestionFeedback/index.js
+++ Learnow Web Server/src/controllers/Feedback/SuggestionFeedback/index.js
@@ -3,9 +3,9 @@ export const SuggestionFeedback = [
         Title: 'Yoga',
         TitleCard: 'Yoga',
         ImgCard: 'yoga',
-        DescriptionCard: `Yoga indicates that in addition to reducing the response to stress,
-it is responsible for calming and restoring balance after the stress and pressure have passed.`,
-        Link: 'https://www.youtube.com/watch?v=YoBwfkB7fI0&t',
+        DescriptionCard: `Yoga reduces the response to stress,
+calming and restoring balance after the stress.`,
+        Link: 'https://www.youtube.com/watch?v=Pz5sO8b9G2o',
         LinkDescription: 'Yoga Video',
         CardNumber: '1'
     },
@@ -13,8 +13,9 @@ it is responsible for calming and restoring balance after the stress and pressur
         Title: 'Pilates',
         TitleCard: 'Pilates',
         ImgCard: 'pilates',
-        DescriptionCard: 'Pilates emphasizes conscious, deep breathing that enhances a sense of relaxation in the body and mind.',
-        Link: 'https://www.youtube.com/watch?v=Dhfv6DWQGlk',
+        DescriptionCard: `Pilates emphasize deep breath
+which improves the sense of relaxation in body and mind.`,
+        Link: 'https://www.youtube.com/watch?v=RDQmnzuE2q4',
         LinkDescription: 'Pilates Video',
         CardNumber: '2'
     },
@@ -22,7 +23,7 @@ it is responsible for calming and restoring balance after the stress and pressur
         Title: 'Music',
         TitleCard: 'Listening To Music',
         ImgCard: 'music',
-        DescriptionCard: 'Listening Music affects learning and hearing skills.',
+        DescriptionCard: 'Listening to music which improve learning and hearing skills.',
         Link: 'https://www.spotify.com/il-en/',
         LinkDescription: 'Listening Music',
         CardNumber: '3'
@@ -32,7 +33,7 @@ it is responsible for calming and restoring balance after the stress and pressur
         TitleCard: 'Running & Aerobic Activity',
         ImgCard: 'run',
         DescriptionCard: 'Running and aerobic activity improve the level of attention and memory.',
-        Link: 'https://www.youtube.com/watch?v=YlbywsBwmME',
+        Link: 'https://www.youtube.com/watch?v=ml6cT4AZdqI',
         LinkDescription: 'Running And Aerobic Activity',
         CardNumber: '4'
     },
@@ -40,7 +41,7 @@ it is responsible for calming and restoring balance after the stress and pressur
         Title: 'Playing',
         TitleCard: 'Playing a Computer Game',
         ImgCard: 'computerGame',
-        DescriptionCard: 'Computer games improve attention, speed of information processing, past flexibility between different tasks and mental rotation of objects.',
+        DescriptionCard: 'Computer games improves attention, speed of information processing and flexible transition between different tasks.',
         Link: 'https://www.miniclip.com/games/en/',
         LinkDescription: 'Playing Computer Game',
         CardNumber: '5'
@@ -49,8 +50,8 @@ it is responsible for calming and restoring balance after the stress and pressur
         Title: 'Dancing',
         TitleCard: 'Dancing',
         ImgCard: 'dancing',
-        DescriptionCard: 'Dancing allows for coping with delayed gratification as well as remembering sequences, while thrilling, which increases concentration and attention.',
-        Link: '',
+        DescriptionCard: 'dance allows to cope with memory sequences which increases concentration and attention.',
+        Link: 'https://www.youtube.com/watch?v=nsSt87J2G8U',
         LinkDescription: 'dancing',
         CardNumber: '6'
     },
@@ -58,8 +59,8 @@ it is responsible for calming and restoring balance after the stress and pressur
         Title: 'Proper nutrition',
         TitleCard: 'Proper nutrition',
         ImgCard: 'nutrition',
-        DescriptionCard: 'Proper nutrition that includes eating vegetables and fruits, fish containing omega 3 and whole grains, with dietary fiber and vitamins helps optimize brain functioning and helps the learning.',
-        Link: '',
+        DescriptionCard: 'Proper nutrition that includes eating vegetables, fruits and fish that contains omega 3 and whole grains, with dietary fiber and vitamins which helps optimize the brain functioning and helps the learning.',
+        Link: 'https://www.nutrition.org.uk/healthyliving/healthydiet.html?__cf_chl_jschl_tk__=9d826aea0df71abdf9cf546d91d32052d7aa20b0-1592336967-0-AQjUw9vWLxjpdStREX2jqH41oeFOJ89KOTW-ncfiB-TbfKhbj5bSLCs8hV-1BHIvl3KvCw6tctKa5v-ci-QhVVt885UGJCAzUvREdS7wzrYlxefRdkOMDs5VxzFxUn5WuE8di5f137TZtsvnpu7TWqgOxbTz7Mcg9WjTsk2V_mRDxTCcNx6eB-tQ23Qg-DS6fhfHdDINcRnev1tRLIELzxdataHng7V2irNBy1tMvjDdlGdWC4yiYjmWtyObduTUwU5vZhg-iGKf5KP04YTa-YfoIHFqhJau7bY4pbchL4CsU08DH-lxwoXBpEuKA3tlxId2jNAbSEYZEqmE4UJTDrEQKQ2bvYOx5qH9hpo0BZwW2hGGY7e8CGpnSGeRhDhEvg',
         LinkDescription: 'Proper nutrition',
         CardNumber: '7'
     },
@@ -67,8 +68,8 @@ it is responsible for calming and restoring balance after the stress and pressur
         Title: 'Swimming',
         TitleCard: 'Swimming',
         ImgCard: 'swimming',
-        DescriptionCard: 'Swimming improves attention level.',
-        Link: '',
+        DescriptionCard: 'Swimming improves the attention level.',
+        Link: 'https://www.swimming.org/justswim/8-benefits-of-swimming/',
         LinkDescription: 'swimming',
         CardNumber: '8'
     },
@@ -77,8 +78,8 @@ it is responsible for calming and restoring balance after the stress and pressur
         TitleCard: 'Mindfulness',
         ImgCard: 'mindfulness',
         DescriptionCard: 'Mindfulness training contributes to the development of basic skills required for the good functioning of all cognitive abilities and among other things to improve the level of attention.',
-        Link: '',
+        Link: 'https://www.youtube.com/watch?v=-2zdUXve6fQ',
         LinkDescription: 'mindfulness',
         CardNumber: '9'
-    },
+    }
 ]
