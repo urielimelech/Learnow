62d67249ce8d68c4c20d9da158ef009d96b23217
uri <urielimelech@hotmail.com>
2020-06-20 23:40:18 +0300

    create a server to get the ip from

--- ThinkGear_Connector/modules/ProxyConnector/index.js
+++ ThinkGear_Connector/modules/ProxyConnector/index.js
@@ -1,38 +1,9 @@
-import axios from 'axios'
 import { spawn } from 'child_process'
 
+import { server } from './connectionServer.js'
 import { learnowUrl } from "./options.js"
-import { socketToWebServer } from "../ConnectionToWebServer/index.js"
-import { WebServerSocketController } from '../SocketToWebServerCtl/index.js'
-
-const getComputerIp = async () => {
-    try{
-        const result = await axios (
-            `http://api.ipify.org:80`
-        )
-        return result.data
-    }
-    catch (e){
-        console.log('catch', e)
-    }
-}
 
 export const startLearnow = async () => {
     const cmd = spawn('cmd.exe', ['/c', 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe', '/new-window', `${learnowUrl}`])
-    var ip = ''
-    var isConnectedToRoom = false
-    if (ip === ''){
-        await getComputerIp()
-        .then(res => ip = res)
-        .catch(err => console.log(err))
-    }
-    if (!isConnectedToRoom && ip !== '') {
-        socketToWebServer.emit('new TGC connection', ip)
-        isConnectedToRoom = true
-    }
-    socketToWebServer.on('open TGC', () => {
-        console.log('call to new tgc')
-            if (isConnectedToRoom)
-                WebServerSocketController(ip)
-        })
+    const serverIP = server
 }
---
+++ ThinkGear_Connector/modules/ProxyConnector/ip.ctl.js
@@ -0,0 +1,46 @@
+import axios from 'axios'
+import { socketToWebServer } from "../ConnectionToWebServer/index.js"
+import { WebServerSocketController } from '../SocketToWebServerCtl/index.js'
+
+var chainedIP = ''
+
+const getComputerIp = async (serial) => {
+    try{
+        const result = await axios (
+            `https://api.ipify.org/`
+        )
+        chainedIP = `${result.data} ${serial}`
+        console.log(chainedIP)
+        if (chainedIP !== '') {
+            socketToWebServer.emit('new TGC connection', chainedIP)
+        }
+        return result.data
+    }
+    catch (e){
+        console.log('catch', e)
+    }
+}
+
+socketToWebServer.on('open TGC', () => {
+    console.log('call to new tgc')
+    WebServerSocketController(chainedIP)
+})
+
+export const getIP = async (req, res, next) => {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    const serialLength = 16
+    var randomSerial = ''
+    var randomNumber
+    for (let i = 0; i < serialLength; i += 1) {
+        randomNumber = Math.floor(Math.random() * chars.length)
+        randomSerial += chars.substring(randomNumber, randomNumber + 1)
+    }
+    try {
+        await getComputerIp(randomSerial)
+        return res.status(200).json({ip: chainedIP})
+    }
+    catch (e){
+        console.log('error get ip', e)
+    }
+    
+}
