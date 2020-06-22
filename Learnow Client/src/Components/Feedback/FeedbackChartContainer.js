de34ebea54f246fbbf3cf5a65b8f49592a67740e
ShohamRoditi <shohamroditi@gmail.com>
2020-06-22 20:30:02 +0300

    create feedback chart

---
+++ Learnow Client/src/Components/Feedback/FeedbackChartContainer.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const StyledFeedbackChartContainer = styled.div`
+background-color: #F4F6F9;
+border: 2px solid #F4F6F9;
+display: flex;
+justify-content: center;
+border-radius: 15px;
+width: 90%;
+height: 32rem;
+margin: 0 auto;
+`
